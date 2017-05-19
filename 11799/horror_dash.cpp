c22e88827820030d44f2e1f0ad1c8d79a76f2ca3
kantuni <henrikh.kantuni@gmail.com>
2017-05-19 07:46:03 -0400

    11799 - Accepted

---
+++ 11799/horror_dash.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int T;
+  cin >> T;
+  for (int t = 1; t <= T; ++t) {
+    int n;
+    cin >> n;
+    
+    int max = 0;
+    while (n--) {
+      int c;
+      cin >> c;
+      if (c > max) {
+        max = c;
+      }
+    }
+    cout << "Case " << t << ": " << max << "\n";
+  }
+  return 0;
+}
