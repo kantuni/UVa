7139a368b46e00101e040ce3ba8ca42a9be55f74
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2017-12-01 22:17:04 -0500

    Update cost_cutting.cpp

--- 11727/cost_cutting.cpp
+++ 11727/cost_cutting.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iterator>
 #include <vector>
 #include <algorithm>
 using namespace std;
@@ -7,10 +6,10 @@ using namespace std;
 int main() {
   int T;
   cin >> T;
-  for (int t = 1; t <= T; ++t) {
+  for (int t = 1; t <= T; t++) {
     vector<int> salaries(3);
     cin >> salaries[0] >> salaries[1] >> salaries[2];
-    sort(begin(salaries), end(salaries));
+    sort(salaries.begin(), salaries.end());
     cout << "Case " << t << ": " << salaries[1] << "\n";
   }
   return 0;
