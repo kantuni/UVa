a8dbfbeda51df7265d8797d851e754a824a69c1f
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-11-13 00:53:30 -0500

    Update and rename event_planning.cpp to event-planning.cpp

--- 11559/event-planning.cpp
+++ 11559/event-planning.cpp
@@ -3,19 +3,16 @@ using namespace std;
 
 int main() {
   int n, b, h, w;
-  
   while (cin >> n >> b >> h >> w) {
     int mc = 2000001;
     int hc = h;
     while (hc--) {
       int p;
       cin >> p;
-      
       int wc = w;
       while (wc--) {
         int a;
         cin >> a;
-        
         if (a >= n) {
           int cost = p * n;
           if (cost < mc) {
@@ -24,7 +21,6 @@ int main() {
         }
       }
     }
-    
     if (mc < b) {
       cout << mc << "\n";
     } else {
