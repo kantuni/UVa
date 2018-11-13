0c3a46784f9222cd1580cd4db936b11873d1b89e
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-11-13 00:54:15 -0500

    Update and rename horror_dash.cpp to horror-dash.cpp

--- 11799/horror-dash.cpp
+++ 11799/horror-dash.cpp
@@ -4,10 +4,9 @@ using namespace std;
 int main() {
   int T;
   cin >> T;
-  for (int t = 1; t <= T; ++t) {
+  for (int t = 1; t <= T; t++) {
     int n;
     cin >> n;
-    
     int max = 0;
     while (n--) {
       int c;
